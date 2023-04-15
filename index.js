407727ac5b2311a41d8f1607eaf435c8186d9c0a
YuriiBurlachenko <burlachenkoyurii@gmail.com>
2023-04-15 22:42:36 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt('Введіть перше число '));
+let num2 = Number(prompt('Введіть друге число 2 '));
+let num3 = Number(prompt('Введіть третє число 3 '));
+alert(
+  `${'Середнє арифметичне трьох чисел:' + '  ' + (num1 + num2 + num3) / 3}`
+);
